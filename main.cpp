b868bd81b35c17fa3c82c35e6a7e03aa49b84501
cba <954577552@qq.com>
2025-05-08 05:13:57 +0800

    第七节课12：49

--- main.cpp
+++ main.cpp
@@ -2,24 +2,115 @@
 // Created by 77361 on 25-5-7.
 //
 #include <windows.h>
+#include <d3d12.h>
+#include <dxgi1_4.h>
+
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+
 
 LPCTSTR gWindowClassName = L"BattleFire";
+ID3D12Device* gD3D12Device = nullptr;
+ID3D12CommandQueue* gD3D12CommandQueue = nullptr;
+IDXGISwapChain3* gSwapChain = nullptr;
+
+bool InitD3D12(HWND inHWND, int inWidth, int inHeight)
+{
+    //dxgifactory->adapter->device->commandQueue->swapchain
+    HRESULT hResult;
+    UINT dxgiFactoryFlags = 0;
+#ifdef _DEBUG
+    {
+        ID3D12Debug* debugController = nullptr;
+        if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
+        {
+            debugController->EnableDebugLayer();
+            dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
+        }
+    }
+#endif
+    // ---------------------创建DXGI工厂------------------------
+    IDXGIFactory4* dxgiFactory;
+    hResult = CreateDXGIFactory2(dxgiFactoryFlags, IID_PPV_ARGS(&dxgiFactory));
+    if (FAILED(hResult))
+    {
+        return false;
+    }
+    // ---------------------创建adapter------------------------
+    IDXGIAdapter1* adapter;
+    int adapterIndex = 0;
+    bool adapterFound = false;
+    while (dxgiFactory->EnumAdapters1(adapterIndex, &adapter) != DXGI_ERROR_NOT_FOUND)
+    {
+        DXGI_ADAPTER_DESC1 desc;
+        adapter->GetDesc1(&desc);
+        if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
+        {
+            continue;
+        }
+        // ---------------------创建device------------------------
+        hResult = D3D12CreateDevice(adapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&gD3D12Device));
+        if (SUCCEEDED(hResult))
+        {
+            adapterFound = true;
+            break;
+        }
+        adapterIndex++;
+    }
+    if (adapterFound == false)
+    {
+        return false;
+    }
+
+    if (FAILED(hResult))
+    {
+        return false;
+    }
+    // ---------------------创建commandqueue------------------------
+    D3D12_COMMAND_QUEUE_DESC d3d12CommandQueueDesc{};
+    hResult = gD3D12Device->CreateCommandQueue(&d3d12CommandQueueDesc, IID_PPV_ARGS(&gD3D12CommandQueue));
+    if (FAILED(hResult))
+    {
+        return false;
+    }
+    // ---------------------创建swapchain----------------------
+    DXGI_SWAP_CHAIN_DESC swapChainDesc{};
+    swapChainDesc.BufferCount = 3;
+    swapChainDesc.BufferDesc = {};
+    swapChainDesc.BufferDesc.Width = inWidth;
+    swapChainDesc.BufferDesc.Height = inHeight;
+    swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    swapChainDesc.OutputWindow = inHWND;
+    swapChainDesc.SampleDesc.Count = 1; //不开启MSAA
+    swapChainDesc.Windowed = true; //是一个窗口程序不全屏
+    swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD; //交换缓冲区时 怎么处理前面这个图片的内容 此处代表丢弃 FLIP支持多缓冲区
+    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+
+    IDXGISwapChain* swapChain = nullptr;
+    dxgiFactory->CreateSwapChain(gD3D12CommandQueue, &swapChainDesc, &swapChain);
+    swapChain->QueryInterface(IID_PPV_ARGS(&gSwapChain));
+    return true;
+}
+
 
 //@param inWParam 里面会存储案件信息
+//@param inLParam createwindowex最后一个参数
 LRESULT CALLBACK WindowProc(HWND inHWND, UINT inMSG, WPARAM inWParam, LPARAM inLParam)
 {
     switch (inMSG)
     {
+    case WM_CREATE:
+        break;
     case WM_DESTROY:
         break;
     case WM_CLOSE:
-        PostQuitMessage(0);//会向消息队列中添加WM_QUIT
+        PostQuitMessage(0); //会向消息队列中添加WM_QUIT
         break;
     }
-    return DefWindowProc(inHWND, inMSG, inWParam, inLParam);//默认的系统处理消息
+    return DefWindowProc(inHWND, inMSG, inWParam, inLParam); //默认的系统处理消息
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int inCmdShow)
 {
     //注册窗口
     WNDCLASSEX wndClassEx;
@@ -34,12 +125,58 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     wndClassEx.hbrBackground = NULL;
     wndClassEx.lpszMenuName = NULL;
     wndClassEx.lpszClassName = gWindowClassName;
+    wndClassEx.lpfnWndProc = WindowProc;
     if (!RegisterClassEx(&wndClassEx))
     {
         MessageBox(nullptr, L"RegisterClassEx failed!", L"Error", MB_OK | MB_ICONERROR);
         return -1;
     }
+    //创建窗口
+    int viewportWidgth = 1280;
+    int viewportHeigth = 720;
+    RECT rect;
+    rect.left = 0;
+    rect.top = 0;
+    rect.right = viewportWidgth;
+    rect.bottom = viewportHeigth;
+    AdjustWindowRect(&rect,WS_OVERLAPPEDWINDOW,FALSE); //此处会修改rect的值 会包含窗口的边框（比如title  bar）
 
+    int windowWidth = rect.right - rect.left;
+    int windowHeight = rect.bottom - rect.top;
 
+    HWND hwnd = CreateWindowEx(NULL, gWindowClassName, L"My Render Window", WS_OVERLAPPEDWINDOW,
+                               CW_USEDEFAULT,CW_USEDEFAULT, windowWidth, windowHeight, NULL, NULL,
+                               hInstance, NULL);
+    if (!hwnd)
+    {
+        MessageBox(nullptr, L"Create Window failed!", L"Error", MB_OK | MB_ICONERROR);
+        return -1;
+    }
+    InitD3D12(hwnd, viewportWidgth, viewportHeigth);
+    ShowWindow(hwnd, inCmdShow);
+    UpdateWindow(hwnd);
+
+    MSG msg;
+    while (true)
+    {
+        ZeroMemory(&msg, sizeof(MSG)); //memset C语言风格
+        //peekmessage
+        //@param1 过滤窗口 null代表接收所有窗口消息
+        //@param2 消息id 0代表不过滤 和后面的参数一个大一个小
+        //@param4 从消息队列中拿到后移除消息
+        if (PeekMessage(&msg, NULL, 0,
+                        0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+            {
+                break;
+            }
+            TranslateMessage(&msg); //从系统消息转换为应用程序能识别的消息
+            DispatchMessage(&msg); //调用dispatchmessage WindowProc就会被调用
+        }
+        else
+        {
+        }
+    }
     return 0;
 }
